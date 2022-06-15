99610c83d3f3b06849af7ad17b8b17de63315cac
psison <sisonpatrick29@yahoo.com>
2022-06-15 18:28:55 -0500

    make controllers directory

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const skills = [
+    {name: 'HTML', proficient: true, _id: 123},
+    {name: 'CSS', proficient: true, _id: 456},
+    {name: 'Javascript', proficient: false, _id: 789},
+]
+
+export {
+    skills
+}
--- routes/skills.js
+++ routes/skills.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
+import { skills } from '../data/skill-data.js'
 
 const router = Router()
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  res.send('respond with a resource')
+  res.render('skills/index', {
+    skills: skills
+  })
 })
 
 export {
