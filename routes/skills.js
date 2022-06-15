7030cedd3d66466a6b7d21940f7442b389ad85b7
psison <sisonpatrick29@yahoo.com>
2022-06-15 18:38:19 -0500

    complete part 1

--- routes/skills.js
+++ routes/skills.js
@@ -1,14 +1,10 @@
 import { Router } from 'express'
-import { skills } from '../data/skill-data.js'
+import * as skillsCtrl from '../controllers/skills.js'
 
 const router = Router()
 
 /* GET users listing. */
-router.get('/', function(req, res) {
-  res.render('skills/index', {
-    skills: skills
-  })
-})
+router.get('/', skillsCtrl.index)
 
 export {
   router
