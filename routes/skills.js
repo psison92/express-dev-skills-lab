2033afb76810ec7d72af74b4603520b18a83cc1d
psison <sisonpatrick29@yahoo.com>
2022-06-16 23:01:25 -0500

    implement delete functionality

--- routes/skills.js
+++ routes/skills.js
@@ -15,6 +15,8 @@ router.get('/:id', skillsCtrl.show)
 // POST -- localhost:3000/skills
 router.post('/', skillsCtrl.create)
 
+// DELETE -- localhost:3000/skills/:id
+router.delete('/:id', skillsCtrl.delete)
 
 export {
   router
