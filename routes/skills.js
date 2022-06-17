53805a701c7125b721c41658a7886b6dc6eaff06
psison <sisonpatrick29@yahoo.com>
2022-06-16 21:27:10 -0500

    complete part 3

--- routes/skills.js
+++ routes/skills.js
@@ -3,10 +3,18 @@ import * as skillsCtrl from '../controllers/skills.js'
 
 const router = Router()
 
-/* GET skills listing. */
+// GET -- localhost:3000/skills 
 router.get('/', skillsCtrl.index)
+
+// GET -- localhost:3000/skills/new
 router.get('/new', skillsCtrl.new)
+
+// GET -- localhost:3000/skills/:id
+router.get('/:id', skillsCtrl.show)
+
+// POST -- localhost:3000/skills
 router.post('/', skillsCtrl.create)
+
 
 export {
   router
